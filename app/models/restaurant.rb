7c0e940ecd96f151df37a4e9601cbfa3e91fe36e
Waihon Yew <yewwaihon@gmail.com>
2016-03-19 18:59:58 +0800

    Add data validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,13 @@
 class Restaurant < ActiveRecord::Base
-  validates :name, presence: true
-
   mount_uploader :image, ImageUploader
 
   has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, allow_blank: true, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
+    message: "must be in the format (123) 456-7890" }
+  validates :website, allow_blank: true, format: { with: /\Ahttps?:\/\/.+\z/,
+    message: "must start with https:// or https://" }
+  validates :address, allow_blank: true, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
+    message: "must be in the format of 350 Firth Avenue, New York, NY 10118"}
 end
